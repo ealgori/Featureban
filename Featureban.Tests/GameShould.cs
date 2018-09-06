e3877fef5f6c5d6fc0e57c5690b659bd4e94cf42
Administrator <Administrator@DESKTOP-0ION1EL>
2018-09-06 21:18:03 +0300

    add game class and test

---
+++ Featureban.Tests/GameShould.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Featureban.Domain;
+using Featureban.Tests.DSL;
+using Xunit;
+
+namespace Featureban.Tests
+{
+    public class GameShould
+    {
+        [Fact]
+        public void JoinGroupOfPlayer_OnCreate()
+        {
+            var playerList = new List<Player>
+            {
+                Create.Player.WithTwoEagleCoin().Build(),
+                Create.Player.WithTwoEagleCoin().Build()
+            };
+
+            var game = new Game(playerList);
+
+            Assert.Equal(playerList[0].Id, game.Players[0].Id);
+            Assert.Equal(playerList[1].Id, game.Players[1].Id);
+
+
+        }
+    }
+}
---
+++ Featureban/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Featureban.Domain.Interfaces;
+
+namespace Featureban.Domain
+{
+    public class Game
+    {
+        public List<Player> Players { get; }
+        public Board Board { get; }
+        public Guid Id { get;}
+        public Game(Guid id, List<Player> players)
+        {
+            Players = players;
+            Id = Guid.NewGuid();
+        }
+        public Game(List<Player> players): this(Guid.NewGuid(), players)
+        {
+        }
+    }
+}
