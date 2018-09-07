b1eb404e5573dab043bc53b8544ea1f4595b04db
Administrator <Administrator@DESKTOP-0ION1EL>
2018-09-07 20:43:47 +0300

    add test UnblockAnotherPlayerBlockedCard_IfDropTails

---
+++ Featureban.Tests/PlayerBehave/UnblockAnotherPlayerCardBehaviourShould.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Featureban.Domain.Enums;
+using Featureban.Domain.PlayerBehave.Single;
+using Featureban.Tests.DSL;
+using Xunit;
+
+namespace Featureban.Tests.PlayerBehave
+{
+    public class UnblockAnotherPlayerCardBehaviourShould
+    {
+        [Fact]
+        public void UnblockAnotherPlayerBlockedCard_IfDropTails()
+        {
+            var unblockAnotherPlayerCardBehaviour = new UnblockAnotherPlayerCardBehaviour();;
+            var playerId = Guid.NewGuid();
+            var card = Create.Card.WhichBlocked().Build();
+            var board = Create.Board.WithCards(card).Build();
+
+            var newBoard = unblockAnotherPlayerCardBehaviour.Apply(playerId, board, CoinSide.Tails);
+
+            Assert.True(unblockAnotherPlayerCardBehaviour.CanApply(playerId, board, CoinSide.Tails));
+            Assert.False(newBoard.Cards.Single().IsBlocked);
+            Assert.NotEqual(playerId, newBoard.Cards.Single().PlayerId);
+        }
+    }
+}
---
+++ Featureban/PlayerBehave/Single/UnblockAnotherPlayerCardBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Featureban.Domain.Enums;
+using Featureban.Domain.PlayerBehave.Interface;
+
+namespace Featureban.Domain.PlayerBehave.Single
+{
+    public class UnblockAnotherPlayerCardBehaviour:IPlayerBehaviour
+    {
+        private readonly Func<Card, Guid, bool> _selector = ((c, id) =>
+            c.IsBlocked
+            && c.PlayerId != id);
+        public bool CanApply(Guid playerId, Board board, CoinSide coinSide)
+        {
+            return coinSide == CoinSide.Tails && board.Cards.Any(c => _selector(c, playerId));
+
+        }
+
+        public Board Apply(Guid playerId, Board board, CoinSide coinSide)
+        {
+            var card = board.Cards.First(c => _selector(c, playerId));
+            var newCard = card.Unblock();
+
+            return board.ReplaceCard(card, newCard);
+        }
+    }
+}
