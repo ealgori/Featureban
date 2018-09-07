716ba7ca05acc9b2d602b90fe348245eb7de4c94
Administrator <Administrator@DESKTOP-0ION1EL>
2018-09-07 12:29:36 +0300

    add test UnblockOwnBlockedCard_IfDropTails

--- Featureban.Tests/PlayerBehaveShould.cs
+++ Featureban.Tests/PlayerBehaveShould.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Featureban.Domain.Enums;
+using Featureban.Domain.PlayerBehave.Single;
+using Featureban.Tests.DSL;
 using Xunit;
 
 namespace Featureban.Tests
@@ -8,9 +12,21 @@ namespace Featureban.Tests
     public class PlayerBehaveShould
     {
         [Fact]
-        public void UnblockBlockedCard_IfDropTails()
+        public void UnblockOwnBlockedCard_IfDropTails()
         {
-            
+            var unblockCardBehaviour = new UnblockOwnCardBahaviour();
+            var playerId = Guid.NewGuid();
+            var card = Create.Card.WhichBlocked().OwnedTo(playerId).Build();
+            var board = Create.Board.WithCards(card).Build();
+
+            var newBoard = unblockCardBehaviour.Apply(playerId, board);
+
+            Assert.True(unblockCardBehaviour.CanApply(playerId,board,CoinSide.Tails));
+            Assert.False(newBoard.Cards.Single().IsBlocked);
+            Assert.Equal(playerId, newBoard.Cards.Single().PlayerId);
         }
+
+
+       
     }
 }
