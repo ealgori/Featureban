904d659059c45dd56ddde5f06b96ee0040380730
Administrator <Administrator@DESKTOP-0ION1EL>
2018-09-07 21:04:41 +0300

    replace test content SkipMove_IfNoBehavioursAcceptable

--- Featureban.Tests/PlayerBehave/PlayerBehaviourShould.cs
+++ Featureban.Tests/PlayerBehave/PlayerBehaviourShould.cs
@@ -13,11 +13,16 @@ namespace Featureban.Tests.PlayerBehave
         [Fact]
         public void SkipMove_IfNoBehavioursAcceptable()
         {
-            var playerBehaviour = Create.PlayerBehavoiur.Build();
+            var playerBehaviour = Create.PlayerBehavoiur
+                .WithTailsBehaviours1Priority(new UnblockOwnCardBahaviour())
+                .WithTailsBehaviours2Priority(new UnblockAnotherPlayerCardBehaviour()).Build();
             var playerId = Guid.NewGuid();
-            var board = Create.Board.Build();
+            var card1 = Create.Card.OwnedTo(playerId).Build();
+            var card2 = Create.Card.Build();
+            var board = Create.Board.WithCards(card1, card2).Build();
 
-            var newBoard = playerBehaviour.Apply(playerId, board, CoinSide.Eagle);
+            var newBoard = playerBehaviour.Apply(playerId, board, CoinSide.Tails);
+
 
             Assert.Equal(board,newBoard);
         }
