17646c421ccbc5947e8ca1428e5c99f67fac6294
Administrator <Administrator@DESKTOP-0ION1EL>
2018-09-07 15:31:15 +0300

    add test MoveOwnUnblockedCard_IfDropTails

---
+++ Featureban.Tests/PlayerBehave/MoveOwnCardForwardBehaviourShould.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Featureban.Domain.Enums;
+using Featureban.Domain.PlayerBehave.Single;
+using Featureban.Tests.DSL;
+using Xunit;
+
+namespace Featureban.Tests.PlayerBehave
+{
+    public class MoveOwnCardForwardBehaviourShould
+    {
+        [Fact]
+        public void MoveOwnUnblockedCard_IfDropTails()
+        {
+            var moveOwnCardBehaviour = new MoveOwnCardForwardBehaviour();
+            var playerId = Guid.NewGuid();
+            var card = Create.Card.OwnedTo(playerId).Build();
+            var board = Create.Board.WithCards(card).Build();
+
+            var newBoard = moveOwnCardBehaviour.Apply(playerId, board);
+
+            Assert.True(moveOwnCardBehaviour.CanApply(playerId, board, CoinSide.Tails));
+            Assert.Equal(CardState.InTesting, newBoard.Cards.Single().State);
+            Assert.Equal(playerId, newBoard.Cards.Single().PlayerId);
+        }
+    }
+}
