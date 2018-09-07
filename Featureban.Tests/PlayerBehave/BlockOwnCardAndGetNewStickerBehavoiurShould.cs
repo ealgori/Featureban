3fcc531a7ed5a0e2fe21f31d00f7e742163a06e3
Administrator <Administrator@DESKTOP-0ION1EL>
2018-09-07 16:16:23 +0300

    add test BlockOwnUnBlockedCardAndGetNewCard_IfDropEagle

---
+++ Featureban.Tests/PlayerBehave/BlockOwnCardAndGetNewStickerBehavoiurShould.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Featureban.Domain.Enums;
+using Featureban.Domain.PlayerBehave.Single;
+using Featureban.Tests.DSL;
+using Xunit;
+
+namespace Featureban.Tests.PlayerBehave
+{
+    public class BlockOwnCardAndGetNewStickerBehavoiurShould
+    {
+        public void BlockOwnUnBlockedCardAndGetNewCard_IfDropEagle()
+        {
+            var blockOwnAndGetNewBehave = new BlockOwnCardBahaviour();
+            var playerId = Guid.NewGuid();
+            var card = Create.Card.OwnedTo(playerId).Build();
+            var board = Create.Board.WithCards(card).Build();
+
+            var newBoard = blockOwnAndGetNewBehave.Apply(playerId, board);
+
+            Assert.True(blockOwnAndGetNewBehave.CanApply(playerId, board, CoinSide.Eagle));
+            Assert.Single(newBoard.Cards, c=>c.IsBlocked);
+            Assert.Single(newBoard.Cards, c=>!c.IsBlocked);
+        }
+
+    }
+}
---
+++ Featureban/PlayerBehave/Multi/BlockOwnAndGetNewSticker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Featureban.Domain.Enums;
+using Featureban.Domain.PlayerBehave.Interface;
+using Featureban.Domain.PlayerBehave.Single;
+
+namespace Featureban.Domain.PlayerBehave.Multi
+{
+    public class BlockOwnAndGetNewSticker:IPlayerBehaviour
+    {
+        readonly IPlayerBehaviour blockOwnBehaviour =  new BlockOwnCardBahaviour();
+        readonly IPlayerBehaviour getNewCardBehaviour = new GetNewCardBahaviour();
+        public BlockOwnAndGetNewSticker()
+        {
+            
+
+        }
+        public bool CanApply(Guid playerId, Board board, CoinSide coinSide)
+        {
+            return blockOwnBehaviour.CanApply(playerId, board, coinSide)
+                   && getNewCardBehaviour.CanApply(playerId, board, coinSide);
+        }
+
+        public Board Apply(Guid playerId, Board board)
+        {
+            var newBoard = blockOwnBehaviour.Apply(playerId, board);
+            var newBoard2 = getNewCardBehaviour.Apply(playerId, board);
+            return newBoard2;
+        }
+    }
+}
