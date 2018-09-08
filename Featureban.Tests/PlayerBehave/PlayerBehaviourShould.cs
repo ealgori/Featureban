5828810bf82a717e3a44af39110cc7519a36bf77
Administrator <Administrator@DESKTOP-0ION1EL>
2018-09-08 12:13:48 +0300

    add test ApplyHigherPriorityBehavior_IfTheyAllApplicable()

--- Featureban.Tests/PlayerBehave/PlayerBehaviourShould.cs
+++ Featureban.Tests/PlayerBehave/PlayerBehaviourShould.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Featureban.Domain.Enums;
+using Featureban.Domain.PlayerBehave.Model;
 using Featureban.Domain.PlayerBehave.Single;
 using Featureban.Tests.DSL;
 using Xunit;
@@ -24,6 +26,34 @@ namespace Featureban.Tests.PlayerBehave
 
 
             Assert.Equal(board,newBoard);
+        }
+
+        [Fact]
+        public void ApplyHigherPriorityBehavior_IfTheyAllApplicable()
+        {
+            var getNewCardBehaviour = new GetNewCardBahaviour();
+            var moveOwnCardBehaviour = new MoveOwnCardForwardBehaviour();
+            var behaviours = new List<PlayerBehaviourContainer>
+            {
+                new PlayerBehaviourContainer(priority: 1, behaviour: getNewCardBehaviour),
+                new PlayerBehaviourContainer(priority: 2, behaviour: moveOwnCardBehaviour),
+
+            };
+            var playerBehaviour = Create.PlayerBehavoiur
+                .WithTailsBehaviours(behaviours)
+                .Build();
+            var playerId = Guid.NewGuid();
+            var card = Create.Card.OwnedTo(playerId).Build();
+            var board = Create.Board.WithCards(card).Build();
+
+            var newBoard = playerBehaviour.Apply(playerId, board, CoinSide.Tails);
+
+
+            Assert.True(getNewCardBehaviour.CanApply(playerId,board,CoinSide.Tails));
+            Assert.True(moveOwnCardBehaviour.CanApply(playerId, board, CoinSide.Tails));
+            Assert.Equal(2, newBoard.Cards.Count());
+            Assert.All(newBoard.Cards, c=> Assert.Equal(playerId, c.PlayerId));
+
         }
 
     }
