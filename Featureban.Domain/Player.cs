62d5aa25a3301589cd68410e5f72379b0b682370
Administrator <Administrator@DESKTOP-0ION1EL>
2018-09-09 18:00:36 +0300

    override equal in Player

--- Featureban.Domain/Player.cs
+++ Featureban.Domain/Player.cs
@@ -7,7 +7,7 @@ using Featureban.Domain.PlayerBehave.Interface;
 
 namespace Featureban.Domain
 {
-    public class Player
+    public class Player: IEquatable<Player>
     {
         private readonly IPlayerBehaviour _behaviour;
         public string Name { get; }
@@ -36,5 +36,41 @@ namespace Featureban.Domain
             return board;
 
         }
+
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Card);
+        }
+
+        public bool Equals(Player obj)
+        {
+            return
+                obj != null
+                && obj.Name == this.Name
+                && obj._behaviour.GetType() == this._behaviour.GetType();
+        }
+
+
+        public static bool operator ==(Player item1, Player item2)
+        {
+            if (object.ReferenceEquals(item1, item2)) { return true; }
+            if ((object)item1 == null || (object)item2 == null) { return false; }
+
+            return item1.Name == item2.Name
+                   && item1.Name == item2.Name;
+        }
+
+        public static bool operator !=(Player item1, Player item2)
+        {
+            return !(item1 == item2);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() & _behaviour.GetHashCode();
+        }
+
+
     }
 }
