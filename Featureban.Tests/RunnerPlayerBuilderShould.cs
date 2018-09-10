cb5bdb2e5285bf742f8b0b2b8c8e225d528c0274
surge_all@bk.ru <alekseygorin@gmail.com>
2018-09-10 17:49:33 +0300

    remove test UseDefaultTailsBehaviorsSettings_OnCreate

--- Featureban.Tests/RunnerPlayerBuilderShould.cs
+++ Featureban.Tests/RunnerPlayerBuilderShould.cs
@@ -14,27 +14,7 @@ namespace Featureban.Tests
 {
     public class RunnerPlayerBuilderShould
     {
-        [Fact]
-        public void UseDefaultTailsBehaviorsSettings_OnCreate()
-        {
-            var expectedTailsBehavioursSequence = new(int priority, Type type )[]
-            {
-                (1,typeof(MoveOwnCardForwardBehaviour)),
-                (2,typeof(GetNewCardBahaviour)),
-                (2,typeof(UnblockOwnCardBahaviour)),
-                (3,typeof(MoveAnotherPlayerCardForwardBehaviour)),
-                (4,typeof(UnblockAnotherPlayerCardBehaviour)),
-            };
-           
-            var playerBuilder = new Runner.DSL.PlayerBuilder();
-            var actualTailsBehavioursSequence = playerBuilder
-                .TailsBehaviours
-                .Select(c=> (c.Priority, c.Behaviour.GetType())
-               );
-
-            Assert.Equal(expectedTailsBehavioursSequence , actualTailsBehavioursSequence);
-
-        }
+      
 
         [Fact]
         public void UseDefaultEagleBehaviorsSettings_OnCreate()
